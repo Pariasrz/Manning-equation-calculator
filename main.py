6b23c034042284c8c68bf5d501013c73131ce8ea
Paria Sarzaeim <59663526+Pariasrz@users.noreply.github.com>
2021-04-10 10:16:53 +0430

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import math
+
+
+q = float(input('Please Enter Discharge \n'))
+b = float(input('Please Enter Bottom Width \n'))
+z = float(input('Please Enter Side Slope \n'))
+s = float(input('Please Enter Longitudnial Slope\n'))
+n = float(input('Please Enter Roughness Coefficient \n'))
+
+k = b**(8/3)*n*q*(s**(-0.5))
+
+if (z == 0):
+    yn = b*(1.155*k+(2/3)*k**0.547)
+    
+else:
+    yn = b*((1.145-0.4*(math.log10(z)))*k**0.61-k/3.5)
+    
+
+if (b == 0):
+    k = n*q*(s**-(0.5))
+    yn = k**(3/8)*z**(-5/8)*(4*(1+z^2))**(1/8)
+    
+yn = 0.5*yn
+
+while(True):
+    yn = yn + 0.01
+    a = (b + z *yn) * yn
+    p = b + 2*yn*(1 + z**2)**0.5
+    r = a / p
+    qc = 1/n*a*r**(2/3)*s**0.5
+    
+    if (qc >= q):
+        print('\n The Depth of The Channel is ', yn)
+        print('\n The Velocity in The Channel is ', q/a)
+    
+    break
